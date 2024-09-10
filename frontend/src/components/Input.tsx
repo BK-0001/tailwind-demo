1e1c391f1980d395f39d1a111683ac8a65fbc76c
Bart Kang <bart.kang.0719@gmail.com>
2024-09-10 11:09:44 -0700

    remove unnecessary customization

--- frontend/src/components/Input.tsx
+++
@@ -1,9 +0,0 @@
-export function Input() {
-  const styles = `
-  bg-transparent py-2 px-4 border w-full mb-10
-  focus:outline-offset-8 focus:outline-4 focus:outline-red-500
-  disabled:bg-gray-200 disabled:cursor-not-allowed
-`;
-
-  return <input type="text" className={styles} />;
-}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { AvatarDemo } from "./components/Avatar";
-import { Input } from "./components/Input";
-import { Button } from "./components/ui/button";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <div className="container p-4 mx-auto">
-      <div className="bg-yui">Yui</div>
-      <div className="dark text-misa p-200 ">Misa</div>
-      <Input />
-      <Button variant={"destructive"} size={"lg"}>
-        Click Me
-      </Button>
-      <AvatarDemo />
-    </div>
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
